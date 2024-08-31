8aeb736d899c6f5c692d4876e3a290ca01518807
Monty <tanmontgomeryf@gmail.com>
2024-08-31 11:58:10 +0800

    husky test

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "test" });
 });
 
 app.listen(port, () => {
